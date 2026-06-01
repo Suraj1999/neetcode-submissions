b3fd28bcdff7070eee6262d67268da3dbfa16377
Suraj1999 <102948555+Suraj1999@users.noreply.github.com>
2026-06-01 15:56:35 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        
+        var setArr = Set<[Int]>()
+        let numsSort = nums.sorted()
+
+        for i in 0..<numsSort.count-2 {
+
+            var left = i+1
+            var right = numsSort.count-1
+
+            while left < right {
+
+                if numsSort[i] + numsSort[left] + numsSort[right] == 0 {
+                    setArr.insert([numsSort[i],numsSort[left],numsSort[right]])
+                    left += 1
+                    right -= 1
+                } else if numsSort[i] + numsSort[left] + numsSort[right] > 0 {
+                    right -= 1
+                } else {
+                    left += 1
+                }
+            }
+        }
+      return Array(setArr)
+    }
+}
